67f2bd86d3a1af80222ff530593cc99d84c741e0
Liane Bez <bez.lianealcantara@gmail.com>
2019-01-15 07:29:18 -0200

    funcionando as estrelas e o coração no click

--- js/description-product.js
+++ js/description-product.js
@@ -1,13 +1,50 @@
 const $heart = document.querySelector(".-heart");
 const $stars = document.querySelectorAll(".star");
-let index = 0;
+const positionLast = $stars.length - 1;
 
 $heart.addEventListener("click", handleClick);
 
-$stars.forEach(function($star) {
-  $star.addEventListener("click", handleClick);
+$stars[0].addEventListener("click", firstStar);
+
+$stars[1].addEventListener("click", function() {
+  middleStar(1);
 });
 
-function handleClick() {
+$stars[2].addEventListener("click", function() {
+  middleStar(2);
+});
+
+$stars[3].addEventListener("click", function() {
+  middleStar(3);
+});
+
+$stars[positionLast].addEventListener("click", lastStar);
+
+function middleStar(index) {
+  $stars.forEach(function($star) {
+    $star.classList.remove("-active");
+  });
+
+  $stars.forEach(function($star, key) {
+    if (key <= index) {
+      $star.classList.add("-active");
+    }
+  });
+}
+
+function firstStar() {
+  $stars.forEach(function($star) {
+    $star.classList.remove("-active");
+  });
+  this.classList.add("-active");
+}
+
+function lastStar(){
+  $stars.forEach(function($star){
+    $star.classList.add("-active");
+  });
+}
+
+function handleClick (){
   this.classList.toggle("-active");
 }
